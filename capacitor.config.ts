5aeb7808fa603e848c6055ddd76da0c21052f5e7
nellwex <nellwex@gmail.com>
2022-07-25 15:57:01 +0800

    init project

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'court.app',
   appName: 'SportCourtApp',
   webDir: 'www',
   bundledWebRuntime: false
