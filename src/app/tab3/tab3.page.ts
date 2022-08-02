3a22dd4a24cb2cc156fc454f9bffbac55eeaad58
nellwex <nellwex@gmail.com>
2022-08-02 10:25:03 +0800

    更新地圖自動定位以及當前位置更新

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,7 +1,8 @@
-import { Component,ViewChild, ElementRef } from '@angular/core';
-import { GoogleMap } from '@capacitor/google-maps';
-
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { GoogleMap, MapType } from '@capacitor/google-maps';
+import { Geolocation, Position } from '@capacitor/geolocation';
 import { environment } from 'src/environments/environment';
+import { __await } from 'tslib';
 
 @Component({
   selector: 'app-tab3',
@@ -10,30 +11,74 @@ import { environment } from 'src/environments/environment';
 })
 export class Tab3Page {
 
-  
+
   @ViewChild('map')
   mapRef: ElementRef;
   map: GoogleMap;
+  postioin: Position;
+  marker: string;
+  showingcLocation: boolean = true;
 
-  constructor() {}
 
-  ionViewDidEnter(){
-this.createMap();
+  constructor() { }
 
+  ionViewDidEnter() {
+    this.createMap();
+    this.locateUser();
+
+  }
+
+  async locateMe(){
+    await this.map.enableCurrentLocation(true);
   }
   
   async createMap() {
+
     this.map = await GoogleMap.create({
       id: 'Map',
       element: this.mapRef.nativeElement,
       apiKey: environment.mapsKey,
       config: {
         center: {
-          lat: 24.17997, 
-          lng: 120.649,
+          lat: 24.180827560886996, 
+          lng: 120.6468631114604,
         },
         zoom: 16,
       },
+      forceCreate: true,
     });
+    await this.addMarkers(this.postioin.coords.latitude, this.postioin.coords.longitude)
+
   }
+
+  async addMarkers(lat,lng) {
+    
+    await this.map.addMarkers([
+      {
+        coordinate:{
+          lat:lat,
+          lng:lng,
+        },
+        draggable: true
+      },
+      {
+        coordinate:{
+          lat: this.postioin.coords.latitude,
+          lng: this.postioin.coords.longitude,
+        },
+        draggable: true
+      },
+    ]);
+
+  }
+
+  async locateUser() {
+
+    const coordinate = await Geolocation.getCurrentPosition();
+this.postioin = coordinate;
+
+  }
+
+
+
 }
